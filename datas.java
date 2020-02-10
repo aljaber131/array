34f44d60b60eb3077d59ac940ff18d1e104e9386
aljaber131 <60335077+aljaber131@users.noreply.github.com>
2020-02-10 08:19:18 -0800

    Writing a Java to search an element from an array

---
+++ datas.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class datas {
+	public static int[] arry = {1,2,3,4,5,6,7,8,9};
+
+
+
+	public static void main(String[] args) {
+		Scanner t= new Scanner(System.in);
+		System.out.println("give your number");
+		int b = t.nextInt();
+		searching(b);
+	}
+	public static void searching (int a) {
+		int i;
+		for(i=0;i<9;i++ ) {
+			if(arry[i]==a) {
+		      System.out.println("found");
+		      break;
+			}
+			else {
+				continue;
+				
+			}
+			
+		}
+	}
+}
